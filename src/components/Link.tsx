14138775b0a02f9ecac0dccc4cd059c8f6fea5f8
artm512 <44886239+artm512@users.noreply.github.com>
2024-12-05 19:06:43 +0900

    レビュー一覧を取得・表示

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -1,7 +1,11 @@
 import { Link as RouterLink } from "react-router-dom";
 
 export const Link = ({ text, to, ...props }: { text: string; to: string }) => (
-  <RouterLink to={to} className="underline hover:no-underline" {...props}>
+  <RouterLink
+    to={to}
+    className="[overflow-wrap:anywhere] underline hover:no-underline"
+    {...props}
+  >
     {text}
   </RouterLink>
 );
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,11 +1,46 @@
+import { useEffect, useState } from "react";
 import { Navigate } from "react-router-dom";
+import { useCookies } from "react-cookie";
 import { Link } from "../../components/Link";
 import { HeadingLevel1 } from "../../components/Heading";
 import { useAuth } from "../../providers/AuthProvider";
+import { api } from "../../utils/api";
+
+type bookReview = {
+  id: string;
+  title: string;
+  url: string;
+  detail: string;
+  review: string;
+  reviewer: string;
+  isMine: boolean;
+};
 
 export const Home = () => {
   const { auth } = useAuth();
-  console.log({ auth });
+  const [cookies] = useCookies();
+  const [bookReviews, setBookReviews] = useState<bookReview[]>([]);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  useEffect(() => {
+    if (auth) {
+      api
+        .get("/books", {
+          params: {
+            offset: "0",
+          },
+          headers: {
+            Authorization: `Bearer ${cookies.token}`,
+          },
+        })
+        .then((res) => {
+          setBookReviews(res.data);
+        })
+        .catch((err) => {
+          setErrorMessage(`リストの取得に失敗しました。${err}`);
+        });
+    }
+  }, [auth]);
 
   // FIXME: ログイン判定リダイレクトを共通化したい
   if (!auth) return <Navigate replace to="/login" />;
@@ -13,43 +48,28 @@ export const Home = () => {
   return (
     <>
       <HeadingLevel1 text="書籍レビュー一覧" />
+      {errorMessage && (
+        <p className="bg-red-200 py-4 px-8 mt-8">{errorMessage}</p>
+      )}
       <ul className="mt-8 flex flex-col gap-4">
-        <li className="shadow-md p-8 rounded-md">
-          <h2 className="text-xl">書籍タイトル</h2>
-          <dl className="mt-4 text-sm">
-            <div className="flex gap-2">
-              <dt>URL:</dt>
-              <dd>
-                <Link to="https://example.com" text="https://example.com" />
-              </dd>
-            </div>
-            <div className="flex gap-2">
-              <dt>レビュワー:</dt>
-              <dd>山田太郎</dd>
-            </div>
-          </dl>
-          <p className="mt-8">
-            書籍レビュー書籍レビュー書籍レビュー書籍レビュー書籍レビュー書籍レビュー書籍レビュー書籍レビュー
-          </p>
-        </li>
-        <li className="shadow-md p-8 rounded-md">
-          <h2 className="text-xl">書籍タイトル</h2>
-          <dl className="mt-4 text-sm">
-            <div className="flex gap-2">
-              <dt>URL:</dt>
-              <dd>
-                <Link to="https://example.com" text="https://example.com" />
-              </dd>
-            </div>
-            <div className="flex gap-2">
-              <dt>レビュワー:</dt>
-              <dd>山田太郎</dd>
-            </div>
-          </dl>
-          <p className="mt-8">
-            書籍レビュー書籍レビュー書籍レビュー書籍レビュー書籍レビュー書籍レビュー書籍レビュー書籍レビュー
-          </p>
-        </li>
+        {bookReviews.map((review) => (
+          <li className="shadow-md p-8 rounded-md" key={review.id}>
+            <h2 className="text-xl">{review.title}</h2>
+            <dl className="mt-4 text-sm">
+              <div className="flex gap-2">
+                <dt>URL:</dt>
+                <dd>
+                  <Link to={review.url} text={review.url} />
+                </dd>
+              </div>
+              <div className="flex gap-2">
+                <dt>レビュワー:</dt>
+                <dd>{review.reviewer}</dd>
+              </div>
+            </dl>
+            <p className="mt-8">{review.review}</p>
+          </li>
+        ))}
       </ul>
     </>
   );
