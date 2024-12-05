1e9f0fe946f2891ed0b6ee79af0c17af7d800c0b
artm512 <44886239+artm512@users.noreply.github.com>
2024-12-05 19:12:29 +0900

    ユニットテスト調整

--- src/pages/login/login.test.tsx
+++ src/pages/login/login.test.tsx
@@ -1,10 +1,15 @@
 import "@testing-library/jest-dom";
+import { MemoryRouter } from "react-router-dom";
 import { render, screen } from "@testing-library/react";
 import { test, expect } from "vitest";
 import { Login } from "./index";
 
 test("ログインボタンが描画されている", () => {
-  render(<Login />);
+  render(
+    <MemoryRouter>
+      <Login />
+    </MemoryRouter>
+  );
   screen.debug();
 
   expect(screen.getByRole("button", { name: "ログイン" })).toBeInTheDocument();
