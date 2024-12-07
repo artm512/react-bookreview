3dae65b8ec17fb596bedfa8b8c78f48ae8009960
artm512 <44886239+artm512@users.noreply.github.com>
2024-12-07 14:47:23 +0900

    ボタンスタイル調整

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -7,7 +7,7 @@ export const ButtonPrimary = ({
   children: ReactNode;
 } & ButtonHTMLAttributes<HTMLButtonElement>) => (
   <button
-    className="text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
+    className="text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5"
     {...props}
   >
     {children}
@@ -21,7 +21,7 @@ export const ButtonSecondary = ({
   children: ReactNode;
 } & ButtonHTMLAttributes<HTMLButtonElement>) => (
   <button
-    className="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 disabled:bg-gray-300 disabled:cursor-not-allowed disabled:opacity-50"
+    className="py-2.5 px-5 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 disabled:bg-gray-300 disabled:cursor-not-allowed disabled:opacity-50"
     {...props}
   >
     {children}
